e7c8fb186a1a95273a8eb7c221c267069e2f5e56
mani3211 <KSUER30043@my.sunywcc.edu>
2025-05-20 21:03:23 -0400

    Changed the file and added the math

--- yourname.py
+++ yourname.py
@@ -11,12 +11,17 @@ user_name = input()
 #step 3, say hi back using the user's name
 print("Hi, " +user_name + ".")
 #step 4, great introduction. Now asking for the user's age
-print("How old are you?")
 #step 5, input the age from the keyboard, then save it to a variable so we can use it later
-user_age = input()
-#step 6, after getting a light introduction to the user,
+user_age = int(input("How old are you"))
+#step 6, greet them
+print("Well it's nice to finally meet you, " +user_name + "!") 
+
+#math we could do is addition, subtraction,
+# multiplecation, divison, moduelo, exponentiation
+half_age = user_age / 2
+print("Half of your age is:", half_age)
 # ask them about their favorite food and add a response
-print("Well it's nice to finally meet you, " +user_name + "!")
 print("You like food right? What is your favorite food?")
 favorite_food = input()
 print("That's awesome! I like " +favorite_food + " aswell!")
+
